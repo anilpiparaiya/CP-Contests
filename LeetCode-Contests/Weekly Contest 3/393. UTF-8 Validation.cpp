69e1564bd15b94ac22a4ef6edbdc999479b7eac6
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-11-05 18:01:36 +0530

    Create 393. UTF-8 Validation.cpp

---
+++ LeetCode-Contests/Weekly Contest 3/393. UTF-8 Validation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int nextRBytes = 0;
+        for(auto x: data){
+            if(nextRBytes == 0){
+                if((x>>5) == 0b110){
+                    nextRBytes = 1;
+                }
+                
+                else if((x >> 4) == 0b1110){
+                    nextRBytes = 2;
+                }
+                
+                else if((x >> 3) == 0b11110){
+                    nextRBytes = 3;
+                }
+                
+                else if((x >> 7) != 0b0){
+                    return false;
+                }
+                
+                
+            }
+            
+            else{
+                if((x >> 6) != 0b10){
+                    return false;
+                }
+                
+                nextRBytes--;
+            }
+        }
+        
+        
+        if(nextRBytes == 0){
+            return true;
+        }
+        
+        return false;
+        
+    }
+};
