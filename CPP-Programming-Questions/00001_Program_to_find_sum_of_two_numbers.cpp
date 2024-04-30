a5d0b4ea6662881910e2bf4c7e98fbd1e95deca6
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-04-30 12:01:27 +0530

    Create 00001_Program_to_find_sum_of_two_numbers.cpp

---
+++ CPP-Programming-Questions/00001_Program_to_find_sum_of_two_numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+  int a, b, s;
+  clrscr();
+  printf("Enter tow no: ")
+  scanf("%d%d", &a, &b);
+  s=a+b;
+  printf("sum=%d", s);
+  getch();
+}
